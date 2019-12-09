ed309f0080048554cd55480ce5d8453af26606c4
agit basar <agitbasar@yahoo.be>
2019-12-09 15:42:10 +0100

    Admin lijst makers en bedrijven

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminService } from './admin.service';
 
 
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
+  ],
+  providers: [
+    AdminService
   ]
 })
 export class AdminModule { }
---
+++ src/app/models/bedrijf.model.ts
@@ -0,0 +1,9 @@
+export class Bedrijf {
+    constructor(
+        public id: number,
+        public naam: string,
+        public adres: string,
+        public biografie: string,
+        public foto: string
+    ){}
+}
