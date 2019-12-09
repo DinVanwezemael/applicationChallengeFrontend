de2a6fbb2405358e2d1acc84ce29d39ac0fd1a98
DinVanwezemael <37238893+DinVanwezemael@users.noreply.github.com>
2019-12-09 10:03:18 +0100

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+const appRoutes: Routes = [
+  //{ path: '', component: HomeComponent },
+  ];
 
 @NgModule({
   declarations: [
@@ -10,7 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: false}),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
