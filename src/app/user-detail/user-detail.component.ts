da4300eda8045ba5d717ccd4737096b38f96c073
Jelle VanLangendonck <jelle.vld@telenet.be>
2019-12-10 09:40:54 +0100

    makerService haal maker op by Id

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+import * as jwtDecode from 'jwt-decode';
+import { MakerService } from '../services/maker.service';
+import { Maker } from '../models/maker.model';
 
 @Component({
   selector: 'app-user-detail',
@@ -10,11 +13,20 @@ export class UserDetailComponent implements OnInit {
   triggered: Boolean = false;
   uploader:FileUploader;
   profielfoto = "https://localhost:44341/images/jelle.jpeg"
-  constructor() { 
+  Maker:Maker;
+  constructor(private _MakerService: MakerService) { 
   }
 
   ngOnInit() {
     this.triggered=false;
+    const token = localStorage.getItem('token')
+    const tokenPayload : any = jwtDecode(token);
+    if(tokenPayload.role = "Maker"){
+      this._MakerService.getMakerWhereId(tokenPayload.GebruikerId).subscribe(result => {
+        this.Maker = result;
+        console.log(result)
+      });
+    }
   }
   openFotoUpload(){
     if(this.triggered===true){
