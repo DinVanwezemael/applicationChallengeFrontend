45204f3330c5391b7d516284199678eae4c340ad
SachaK-r0697191 <r0697191@student.thomasmore.be>
2019-12-11 22:30:21 +0100

    login merge

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { interval } from 'rxjs';
+declare var $: any;
 
 @Component({
   selector: 'app-register',
@@ -13,7 +15,7 @@ export class RegisterComponent implements OnInit {
   registerFormBedrijf: FormGroup;
   form: number;
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.registerFormStudent.value);
   }
 
@@ -24,30 +26,41 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.registerFormStudent = this.fb.group({
       voornaam: new FormControl('', Validators.required),
-    achternaam: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    wachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    herhaalWachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      achternaam: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      wachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      herhaalWachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
 
     this.registerFormBedrijf = this.fb.group({
       voornaam: new FormControl('', Validators.required),
-    achternaam: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    wachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    herhaalWachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      achternaam: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      wachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      herhaalWachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
 
     this.form = 1;
 
-    if(this.registerFormStudent.controls['wachtwoord'].value == this.registerFormStudent.controls['herhaalWachtwoord'].value){
-      
+    if (this.registerFormStudent.controls['wachtwoord'].value == this.registerFormStudent.controls['herhaalWachtwoord'].value) {
+
     }
+
+    $(document).ready(function () {
+      $('.carousel').carousel({
+        touch: false,
+        interval: false,
+      })
+    })
   }
 
   changeForm(nr: number) {
     this.form = nr;
     console.log(this.form);
+  }
+
+  registerStudentNext() {
+    $('.carousel').carousel(1)
   }
 
 }
