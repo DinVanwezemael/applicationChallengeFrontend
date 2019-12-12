18b5cc962e6fcef569649bec0888c164f53cb057
SachaK-r0697191 <r0697191@student.thomasmore.be>
2019-12-12 02:30:41 +0100

    Maker registraten, validation, forms

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { interval } from 'rxjs';
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { AuthenticateService } from '../services/authenticate.service';
 declare var $: any;
 
 @Component({
@@ -10,10 +12,13 @@ declare var $: any;
 })
 export class RegisterComponent implements OnInit {
   @Output() login = new EventEmitter<boolean>();
-  constructor(private fb: FormBuilder) { }
+  form: number;
+  submitted = false;
+  wait = false;
+
+  constructor(private fb: FormBuilder, private parserFormatter: NgbDateParserFormatter, private authenticateService: AuthenticateService) { }
   registerFormStudent: FormGroup;
   registerFormBedrijf: FormGroup;
-  form: number;
 
   onSubmit() {
     console.log(this.registerFormStudent.value);
@@ -27,10 +32,21 @@ export class RegisterComponent implements OnInit {
     this.registerFormStudent = this.fb.group({
       voornaam: new FormControl('', Validators.required),
       achternaam: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      wachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      herhaalWachtwoord: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+      geboortedatum: new FormControl({}, Validators.required),
+      straat: new FormControl('', Validators.required),
+      nr: new FormControl('', Validators.required),
+      stad: new FormControl('', Validators.required),
+      postcode: new FormControl(null, Validators.required),
+      biografie: new FormControl(''),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordHerhaald: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    },
+      {
+        validator: MustMatch('password', 'passwordHerhaald')
+      }
+    )
 
     this.registerFormBedrijf = this.fb.group({
       voornaam: new FormControl('', Validators.required),
@@ -42,14 +58,11 @@ export class RegisterComponent implements OnInit {
 
     this.form = 1;
 
-    if (this.registerFormStudent.controls['wachtwoord'].value == this.registerFormStudent.controls['herhaalWachtwoord'].value) {
-
-    }
-
     $(document).ready(function () {
       $('.carousel').carousel({
         touch: false,
         interval: false,
+        wrap: false
       })
     })
   }
@@ -61,6 +74,87 @@ export class RegisterComponent implements OnInit {
 
   registerStudentNext() {
     $('.carousel').carousel(1)
+    $('.carousel').carousel('pause');
   }
 
+  registerStudentBack() {
+    $('.carousel').carousel(0)
+    $('.carousel').carousel('pause');
+  }
+
+  onSubmitStudent() {
+    this.wait = true;
+    this.submitted = true;
+
+    if (this.registerFormStudent.invalid) {
+      $('.carousel').carousel(0)
+      $('.carousel').carousel('pause');
+      this.wait = false;
+      return;
+    }
+
+    //13/11/1998 00:00:00
+
+    var datum = this.registerFormStudent.value.geboortedatum.day + '/' + 
+                this.registerFormStudent.value.geboortedatum.month + '/' + 
+                this.registerFormStudent.value.geboortedatum.year + ' 00:00:00'
+
+    var maker = {
+      voornaam: this.registerFormStudent.value.voornaam,
+      achternaam: this.registerFormStudent.value.achternaam,
+      biografie: this.registerFormStudent.value.biografie,
+      geboorteDatum: datum,
+      nr: this.registerFormStudent.value.nr,
+      postcode: this.registerFormStudent.value.postcode,
+      stad: this.registerFormStudent.value.stad,
+      straat: this.registerFormStudent.value.straat,
+    }
+
+    var userLogin = {
+      username: this.registerFormStudent.value.username,
+      password: this.registerFormStudent.value.password,
+      email: this.registerFormStudent.value.email,
+    }
+
+/*     this.authenticateService.addMaker(maker).subscribe(result => {
+      console.log("yeet")
+    },
+    err => {
+      console.log(err);
+    }) */
+
+    this.authenticateService.addMaker(maker, userLogin).subscribe(result => {
+      console.log("yeet");
+      console.log("result");
+    },
+    err => {
+      console.log(err);
+    })
+
+    console.log(maker);
+    console.log(userLogin);
+    this.wait = false;
+  }
+
+  get f() { return this.registerFormStudent.controls; }
+
+}
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
 }
--- src/app/authentication/services/authenticate.service.ts
+++ src/app/authentication/services/authenticate.service.ts
@@ -68,4 +68,8 @@ export class AuthenticateService {
   editUsername(userid: number, user: User) {
     return this._httpClient.put<User>("https://localhost:44341/api/userLogin/" + userid, user);
   }
+
+  addMaker(maker: any, login: any) {
+    return this._httpClient.post<any>("https://localhost:44341/api/userLogin/AddLoginMaker", maker, login);
+  }
 }
