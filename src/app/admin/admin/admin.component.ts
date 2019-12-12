d61ddfd53b530ecdc59fa4e3982ecb8ace66e45d
agit basar <agitbasar@yahoo.be>
2019-12-12 15:49:11 +0100

    pull request

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -6,6 +6,7 @@ import { Review } from 'src/app/models/review.model';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { NgbRatingConfig, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Opdracht } from 'src/app/models/opdracht.model';
 
 @Component({
   selector: 'app-admin',
@@ -16,13 +17,15 @@ export class AdminComponent implements OnInit {
   makers: Maker[] = [];
   bedrijven: Bedrijf[] = [];
   reviews: Review[] = [];
+  opdrachten: Opdracht[] = [];
   searchText;
-  emailFilter: any = { email: '' };
-  gebruiksnaamFilter: any = { nickname: '' };
+  makerFilter: any = { nickname: '', email: '' };
   bedrijfFilter: any = { naam: '' };
   reviewerFilter =  { maker: {nickname: '' }, reviewTekst: ''};
+  opdrachtFilter =  { titel: ''};
   closeResult: string;
   review: Review;
+  opdracht: Opdracht;
 
   reviewForm = this.fb.group({
     reviewTekst: ['']
@@ -42,15 +45,29 @@ export class AdminComponent implements OnInit {
       this.bedrijven = result;
     })
 
-    /* this._adminService.getReviews().subscribe(result => {
+    this._adminService.getReviews().subscribe(result => {
       this.reviews = result;
-    }) */
+    })
+    
+    this._adminService.getOpdrachten().subscribe(result => {
+      this.opdrachten = result;
+    })
   }
 
-  open(content, r: Review) {
+  reviewModal(content, r: Review) {
     this.review = r;
     console.log(this.review);
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, {ariaLabelledBy: 'review'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  opdrachtModal(contentOpdracht, o: Opdracht) {
+    this.opdracht = o;
+    console.log(this.opdracht);
+    this.modalService.open(contentOpdracht, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -63,7 +80,7 @@ export class AdminComponent implements OnInit {
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
-      return  `with: ${reason}`;
+      return  `with: ${reason}`;    
     }
   }
 
@@ -71,6 +88,9 @@ export class AdminComponent implements OnInit {
     this.router.navigate(['makerForm'])
   }
 
+  updateOpdracht(o: Opdracht){
+    console.log(o)
+  }
   reviewDetail(r: Review){
     console.log(r)
   }
